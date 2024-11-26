01a2c8b66ab3031db838defafeba0f47a7d22dc0
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2024-11-26 13:36:55 +0100

    Update TaskDetails.jsx

--- src/Components/TaskDetails.jsx
+++ src/Components/TaskDetails.jsx
@@ -138,7 +138,7 @@ const TaskDetails = () => {
               </>
             ) : (
               <p className="text-gray-500">
-                You can only monitor the progress of this task. <br>Progress: {task.status}</br>
+                You can only monitor the progress of tasks assigned to other users. <br/> Task : {task.status}
               </p>
             )}
           </>
