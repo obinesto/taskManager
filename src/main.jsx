ce91656c22876347e652c3398b74e7de2b951711
Cyprian Obi <obicyprian180@gmail.com>
2024-12-26 14:53:12 +0100

    update wdyr

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./Utils/wdyr.js"
+// import "./Utils/wdyr.js"; // Not needed in production. Uncomment for development.
 import './index.css'
 import App from './App.jsx'
 
