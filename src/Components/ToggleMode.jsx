83b92e1098aeeb8390dcd2a8d60a2a2650539a77
Cyprian Obi <obicyprian180@gmail.com>
2025-01-09 10:24:20 +0100

    update paths

--- src/Components/ToggleMode.jsx
+++ src/Components/ToggleMode.jsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 
-import { Button } from "../components/ui/button";
+import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
